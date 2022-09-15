0027b58e89c1041e0e61ab432a16d375b58a2b43
vegetablefried <63182566+vegetablefried@users.noreply.github.com>
2022-09-16 04:19:39 +0900

    MOR - 회원가입 논리적 오류 해결(2022/9/16)

--- MyOwnRepository/src/main/java/com/co/dao/MemberDAOImpl.java
+++ MyOwnRepository/src/main/java/com/co/dao/MemberDAOImpl.java
@@ -22,51 +22,34 @@ public class MemberDAOImpl implements MemberDAO {
     private static final String Namespace = "com.co.mapper.memberMapper";
     
     @Override
-    public List<MemberVO> selectMember() throws Exception {
+    public List<MemberVO> selectMember() throws Exception {				// 회원 조회
  
         return sqlSession.selectList(Namespace+".selectMember");
     }
 
 	@Override
-	public void joinMember(MemberVO member) throws Exception {
+	public void joinMember(MemberVO member) throws Exception {			// 회원가입
 		// TODO Auto-generated method stub
 		sqlSession.insert(Namespace+".joinMember", member);
 		
 	}
 
 	@Override
-	public int idCheck(String id) throws Exception {
+	public int idCheck(String id) throws Exception {				// 아이디 중복 체크
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne(Namespace+".idCheck", id);
 	}
 
 	@Override
-	public LoginDTO selectPerson(String sessionID) throws Exception {
+	public LoginDTO selectPerson(String sessionID) throws Exception {		// 로그인
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne(Namespace+".selectPerson");
 	}
-    
 
- 
-
-/*
 	@Override
-	public MemberVO loginMember(MemberVO member) {
+	public int nicknameCheck(String nickname) throws Exception {		// 닉네임 중복 체크
 		// TODO Auto-generated method stub
-		MemberVO res = null;
-		
-		try {
-			res = sqlSession.selectOne(Namespace+".loginMember", member);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return res;
+		return sqlSession.selectOne(Namespace+".nicknameCheck", nickname);
 	}
-*/
 
-
-
-
-
- 
 }
